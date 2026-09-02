cd2af6d312efbae379bb2fa01bc068f3016247d3
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2026-09-02 12:36:49 +0530

    bridged communication between local python server and browser vice-versa

--- python/server.py
+++ python/server.py
@@ -5,6 +5,29 @@ import websockets
 async def handler(websocket):
     print("Browser connected")
 
+    async def send_commands():
+        await asyncio.sleep(3)
+
+        print("Moving robot forward")
+
+        await websocket.send(
+            json.dumps({
+                "forward": True
+            })
+        )
+
+        await asyncio.sleep(2)
+
+        await websocket.send(
+            json.dumps({
+                "forward": False
+            })
+        )
+
+        print("Robot stopped")
+
+    asyncio.create_task(send_commands())
+
     async for message in websocket:
         data = json.loads(message)
 
@@ -13,13 +36,6 @@ async def handler(websocket):
             f"z={data['z']:.2f} "
             f"rot={data['rotationY']:.2f}"
         )
-
-        # Example command
-        # await websocket.send(
-        #     json.dumps({
-        #         "forward": True
-        #     })
-        # )
 
 async def main():
     async with websockets.serve(handler, "localhost", 8765):
